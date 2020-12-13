1ade6227d7f3be00cdc57a9256d24ab17a70ebd7
alaa ben hassine <alaa@tekyn.com>
2020-12-13 17:01:52 +0100

    home page

--- library/tech_library/urls.py
+++ library/tech_library/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from django.conf.urls import url
+from tech_library.views import home
 #add app urls here
-urlpatterns = []
+
+urlpatterns = [url(r'^$',home,name="home"),]
--- library/tech_library/views.py
+++ library/tech_library/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, Http404
 
-# Create your views here.
+def home(request):
+    page_title = "Home" 
+    content = "In this site we ..."
+    menu = {"Developers":"developers","Tech Field":"tech_field","Home":""}
+    if not menu:
+        raise Http404
+    return render(request, 'tech_library/home.html', locals())
+
