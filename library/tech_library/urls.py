cedde5fdda7618b38d665898f2c96a71a4caeb71
alaa ben hassine <alaa@tekyn.com>
2020-12-13 21:54:34 +0100

    displa themes + dev details + topic + ,topic details

--- library/tech_library/urls.py
+++ library/tech_library/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from django.conf.urls import url
-from tech_library.views import home, display_dev
+from tech_library.views import home, display_dev, display_dev_details, display_themes, display_themes_details, display_topic_details
 #add app urls here
 
 urlpatterns = [
     url(r'^$',home,name="home"),
     url(r'^developers/$',display_dev,name="display_dev"),
-    url(r'^developers/(\d+)$',display_dev,name="developer_detatils"),
+    url(r'^themes/$',display_themes,name="display_themes"),
+    url(r'^developers/(\d+)$',display_dev_details,name="developer_detatils"),
+    url(r'^themes/(\d+)$',display_themes_details,name="display_themes_details"),
+    url(r'^topics/(\d+)$',display_topic_details,name="display_topic_details"),
     ]
--- library/tech_library/views.py
+++ library/tech_library/views.py
@@ -16,3 +16,32 @@ def display_dev(request):
         raise Http404
     return render(request, 'tech_library/developers.html', locals())
 
+def display_themes(request):
+    themes  = Theme.objects.all()
+    page_title = "Themes" 
+    content = "List of Themes:"
+    if not themes:
+        raise Http404
+    return render(request, 'tech_library/themes.html', locals())
+
+
+def display_dev_details(request, id_dev):
+    dev_details =Developer.objects.get(id=id_dev)
+    topics = dev_details.topic_set.all()
+    return render(request, 'tech_library/developer_details.html', locals())
+
+def display_themes_details(request, id_theme):
+    theme  = Theme.objects.get(id=id_theme)
+    topics = list(theme.topic_set.all())
+
+    return render(request, 'tech_library/theme_details.html', locals())
+
+
+def display_topic_details(request, id_theme):
+    topic  = Topic.objects.get(id=id_theme)
+    devs = topic.developer.all()
+    themes = topic.theme.all()
+    return render(request, 'tech_library/topic_details.html', locals())
+
+
+
