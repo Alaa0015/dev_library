50474c7e469fe0be3aa247eb5b8868be39f28496
alaa ben hassine <alaa@tekyn.com>
2020-12-13 19:25:15 +0100

    create models

--- library/tech_library/admin.py
+++ library/tech_library/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from tech_library.models import Developer, Topic, Theme
+
+admin.site.register(Developer)
+admin.site.register(Theme)
+admin.site.register(Topic)
--- library/tech_library/models.py
+++ library/tech_library/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Developer(models.Model):
+    name = models.CharField(max_length=42)
+    description = models.CharField(max_length=124)
+    def __str__(self):
+        return self.name
+
+
+class Theme(models.Model):
+    name = models.CharField(max_length=42)
+    description = models.CharField(max_length=124)
+    def __str__(self):
+        return self.name
+
+class Topic(models.Model):
+    title = models.CharField(max_length=42)
+    description = models.CharField(max_length=124)
+    developer = models.ManyToManyField(Developer)
+    theme = models.ManyToManyField(Theme)
+    def __str__(self):
+        return self.title
