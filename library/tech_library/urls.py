0a0396f4b954f5e6217ed218eec2f12596347c1a
alaa ben hassine <alaa@tekyn.com>
2020-12-13 20:29:33 +0100

    Display all developers

--- library/tech_library/urls.py
+++ library/tech_library/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from django.conf.urls import url
-from tech_library.views import home
+from tech_library.views import home, display_dev
 #add app urls here
 
-urlpatterns = [url(r'^$',home,name="home"),]
+urlpatterns = [
+    url(r'^$',home,name="home"),
+    url(r'^developers/$',display_dev,name="display_dev"),
+    url(r'^developers/(\d+)$',display_dev,name="developer_detatils"),
+    ]
--- library/tech_library/views.py
+++ library/tech_library/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, Http404
-
+from tech_library.models import Developer, Theme, Topic
 def home(request):
     page_title = "Home" 
     content = "In this site we ..."
-    menu = {"Developers":"developers","Tech Field":"tech_field","Home":""}
-    if not menu:
-        raise Http404
+
     return render(request, 'tech_library/home.html', locals())
 
+
+def display_dev(request):
+    developers  = Developer.objects.all()
+    page_title = "Developers" 
+    content = "List of developers:"
+    if not developers:
+        raise Http404
+    return render(request, 'tech_library/developers.html', locals())
